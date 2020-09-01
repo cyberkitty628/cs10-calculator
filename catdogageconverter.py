8fd9d190e094af0caf418715d7880a7e9f3ade4c
Kristina <cyberkitty628@gmail.com>
2020-09-01 16:23:18 -0700

    catdogageconverter.py

--- catdogageconverter.py
+++ catdogageconverter.py
@@ -9,3 +9,15 @@ cat_age = int(cat_age)
 #Since the human equivalent of a cat's age is approximately seven times greater, 
 #we will now have to assign the human age as the cat's age multiplied by 7.
 human_age = cat_age * 7
+
+#Now, we will call on the print function to return the user's cat's age in human years. 
+#In order to concatenate the human_age number with the preceding string, we will have to convert it with the str() function.
+print("Your cat's age in human years equals approx. " + str(human_age))
+
+#To get the dog's human age, we will repeat the previous steps but replacing 7 with 15 as dogs are about double the age of cats in human years.
+dog_age = input("How old is your dog?")
+dog_age = int(dog_age)
+
+human_age = dog_age * 15
+
+print("Your dog's age in human years equals approx. " + str(human_age))
