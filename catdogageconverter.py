345035e14e510b7c795ab85995ac5fe669822695
Kristina <cyberkitty628@gmail.com>
2020-09-01 16:09:33 -0700

    catdogageconverter.py

--- catdogageconverter.py
+++ catdogageconverter.py
@@ -1,1 +1,7 @@
 print("Welcome to the kitty & puppy age converter!")
+
+# First, we will create an input field to get the age of the user's cat. 
+# Since the input function will always return a string, we have to change the type of the user's input to an integer. 
+
+cat_age = input("How old is your cat?")
+cat_age = int(cat_age)
